7d0c0ed3c0507f1d3d93e45b23d4f0c61e3ae45d
marksymms <msymms@gmail.copm>
2018-08-19 15:16:32 -0500

    Deletion of some themes

--- wp-content/themes/vantage/functions.php
+++ wp-content/themes/vantage/functions.php
@@ -7,7 +7,7 @@
  * @license GPL 2.0
  */
 
-define( 'SITEORIGIN_THEME_VERSION', '1.7.9' );
+define( 'SITEORIGIN_THEME_VERSION', '1.7.10' );
 define( 'SITEORIGIN_THEME_JS_PREFIX', '.min' );
 define( 'SITEORIGIN_THEME_CSS_PREFIX', '.min' );
 
